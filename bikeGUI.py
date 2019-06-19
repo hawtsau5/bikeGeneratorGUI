8bcbd576a596a9df029d20cfc5dc0c66286045d6
hawtsau5 <sasha.ivana9@gmail.com>
2019-06-19 09:56:09 -0400

    one file, tidy

---
+++ bikeGUI.py
@@ -0,0 +1,187 @@
+from tkinter import *
+from tkinter import ttk
+import tkinter as tk
+import time
+import math
+import RPi.GPIO as GPIO
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+GPIO.setup(21, GPIO.IN)
+
+def calculate_elapse(channel):              # callback function
+    global pulse, start_timer, elapse
+    pulse+=1                                # increase pulse by 1 whenever interrupt occurred
+    elapse = time.time() - start_timer      # elapse for every 1 complete rotation made!
+    start_timer = time.time()               # let current time equals to start_timer
+
+def calculate_speed(circ_cm):
+    global pulse,elapse,rpm,dist_km,dist_meas,km_per_sec,km_per_hour
+    if elapse !=0:                          # to avoid DivisionByZero error
+        rpm = 1/elapse * 60          
+        dist_km = circ_cm/100000            # convert cm to km
+        km_per_sec = dist_km / elapse       # calculate KM/sec
+        km_per_hour = km_per_sec * 3600     # calculate KM/h
+        dist_meas = (dist_km*pulse)*1000    # measure distance traverse in meter
+        return km_per_hour
+
+def init_interrupt():
+    GPIO.add_event_detect(sensor, GPIO.FALLING, callback = calculate_elapse, bouncetime = 20)
+
+def start_RPM():
+    init_interrupt()
+    start = time.time()
+    while True:
+        a.update()
+        calculate_speed(20) # call this function with wheel radius as parameter
+        print('rpm:{0:.0f}-RPM kmh:{1:.0f}-KMH dist_meas:{2:.2f}m pulse:{3}'.format(rpm,km_per_hour,dist_meas,pulse))
+        time.sleep(0.1)
+        total_time = time.time() - start
+        update_data(round(rpm,2), round(km_per_hour,2),round(dist_meas,2), round(total_time,2))
+        time.sleep(1)
+
+def click():
+    name = name_entry.get()
+    age = age_entry.get()
+    live_window(name, age)
+
+def live_window(x,y):
+    enter_name.destroy()                         
+    enter_age.destroy()                     #destroy all whidgets
+    enter_button.destroy()                            
+    name_entry.destroy()
+    age_entry.destroy()
+
+    create_live_whidgets(x,y)
+    place_live_whidgets()
+    start_RPM()
+    
+def update_data(w,x,y,z):
+    a.rpm_data_label["text"] = w
+    a.speed_data_label["text"] = x
+    a.distance_data_label["text"] = y
+    a.elapsed_data_time["text"] = z
+
+def create_live_whidgets(x,y): 
+    a.first_frame         = tk.LabelFrame(a, text='Power Levels', font='Helvetica 22', bd=border, bg=frameBG, fg=frameTC)
+    a.power_label         = tk.Label(a.first_frame, text="power :", font=frameFont, bg=frameBG, fg=frameTC) #create power labels
+    a.voltage_label       = tk.Label(a.first_frame, text="voltage :", font=frameFont, bg=frameBG, fg=frameTC)
+    a.current_label       = tk.Label(a.first_frame, text="current :", font=frameFont, bg=frameBG, fg=frameTC)
+    a.battery_label       = tk.Label(a.first_frame, text="battery :", font=frameFont, bg=frameBG, fg=frameTC)
+    a.power_data_label    = tk.Label(a.first_frame, text="   ", font=frameFont, bg=frameBG, fg=frameTC) #create rpm labels
+    a.voltage_data_label  = tk.Label(a.first_frame, text="   ", font=frameFont, bg=frameBG, fg=frameTC)
+    a.current_data_label  = tk.Label(a.first_frame, text="    ", font=frameFont, bg=frameBG, fg=frameTC)
+    a.battery_data_label  = tk.Label(a.first_frame, text="    ", font=frameFont, bg=frameBG, fg=frameTC)
+    a.power_unit_label    = tk.Label(a.first_frame, text="W", font=frameFont, bg=frameBG, fg=frameTC) #create rpm labels
+    a.voltage_unit_label  = tk.Label(a.first_frame, text="V", font=frameFont, bg=frameBG, fg=frameTC)
+    a.current_unit_label  = tk.Label(a.first_frame, text="A", font=frameFont, bg=frameBG, fg=frameTC)
+    a.battery_unit_label  = tk.Label(a.first_frame, text="%", font=frameFont, bg=frameBG, fg=frameTC)
+
+    a.second_frame        = tk.LabelFrame(a, text='Speed Levels', font='Helvetica 22', bd=border, bg=frameBG, fg=frameTC)
+    a.rpm_label           = tk.Label(a.second_frame, text="RPM :", font=frameFont, bg=frameBG, fg=frameTC)
+    a.speed_label         = tk.Label(a.second_frame, text="speed :", font=frameFont, bg=frameBG, fg=frameTC)
+    a.distance_label      = tk.Label(a.second_frame, text="distance :", font=frameFont, bg=frameBG, fg=frameTC)
+    a.elapsed_time        = tk.Label(a.second_frame, text="time elapsed :", font=frameFont, bg=frameBG, fg=frameTC)
+    a.rpm_data_label      = tk.Label(a.second_frame, text="    ", font=frameFont, bg=frameBG, fg=frameTC)
+    a.speed_data_label    = tk.Label(a.second_frame, text="  ", font=frameFont, bg=frameBG, fg=frameTC)
+    a.distance_data_label = tk.Label(a.second_frame, text=" ", font=frameFont, bg=frameBG, fg=frameTC)
+    a.elapsed_data_time   = tk.Label(a.second_frame, text="   ", font=frameFont, bg=frameBG, fg=frameTC)
+    a.rpm_unit_label      = tk.Label(a.second_frame, text="rpms", font=frameFont, bg=frameBG, fg=frameTC)
+    a.speed_unit_label    = tk.Label(a.second_frame, text="mph", font=frameFont, bg=frameBG, fg=frameTC)
+    a.distance_unit_label = tk.Label(a.second_frame, text="m", font=frameFont, bg=frameBG, fg=frameTC)
+    a.elapsed_unit_time   = tk.Label(a.second_frame, text="s", font=frameFont, bg=frameBG, fg=frameTC)
+
+
+    a.third_frame         = tk.LabelFrame(a, text='User Info', font='Helvetica 22', bd=border, bg=frameBG, fg=frameTC)
+    a.name_label          = tk.Label(a.third_frame, text="name :", font=frameFont, bg=frameBG, fg=frameTC)
+    a.age_label           = tk.Label(a.third_frame, text="age :", font=frameFont, bg=frameBG, fg=frameTC)
+    a.heart_label         = tk.Label(a.third_frame, text="heart rate :", font=frameFont, bg=frameBG, fg=frameTC)
+    a.calorie_label       = tk.Label(a.third_frame, text="calories burned :", font=frameFont, bg=frameBG, fg=frameTC)
+    a.name_data_label     = tk.Label(a.third_frame, text=x, font=frameFont, bg=frameBG, fg=frameTC)
+    a.age_data_label      = tk.Label(a.third_frame, text=y, font=frameFont, bg=frameBG, fg=frameTC)
+    a.heart_data_label    = tk.Label(a.third_frame, text="   ", font=frameFont, bg=frameBG, fg=frameTC)
+    a.calorie_data_label  = tk.Label(a.third_frame, text="  ", font=frameFont, bg=frameBG, fg=frameTC)
+    a.heart_unit_label    = tk.Label(a.third_frame, text="bpm", font=frameFont, bg=frameBG, fg=frameTC)
+    
+
+def place_live_whidgets():
+    a.first_frame.grid(row=1, column=1)
+    a.power_label.grid(row=1, column=1)
+    a.voltage_label.grid(row=2, column=1)
+    a.current_label.grid(row=3, column=1)
+    a.battery_label.grid(row=4, column=1)
+    a.power_data_label.grid(row=1, column=2)
+    a.voltage_data_label.grid(row=2, column=2)
+    a.current_data_label.grid(row=3, column=2)
+    a.battery_data_label.grid(row=4, column=2)
+    a.power_unit_label.grid(row=1, column=3)
+    a.voltage_unit_label.grid(row=2, column=3)
+    a.current_unit_label.grid(row=3, column=3)
+    a.battery_unit_label.grid(row=4, column=3)
+
+    a.second_frame.grid(row=1, column=2)
+    a.rpm_label.grid(row=1, column=1)
+    a.speed_label.grid(row=2, column=1)
+    a.distance_label.grid(row=3, column=1)
+    a.elapsed_time.grid(row=4, column=1)
+    a.rpm_data_label.grid(row=1, column=2)
+    a.speed_data_label.grid(row=2, column=2)
+    a.distance_data_label.grid(row=3, column=2)
+    a.elapsed_data_time.grid(row=4, column=2)
+    a.rpm_unit_label.grid(row=1, column=3)
+    a.speed_unit_label.grid(row=2, column=3)
+    a.distance_unit_label.grid(row=3, column=3)
+    a.elapsed_unit_time.grid(row=4, column=3)
+
+    a.third_frame.grid(row=1, column=3)
+    a.name_label.grid(row=1, column=1)
+    a.age_label.grid(row=2, column=1)
+    a.heart_label.grid(row=3, column=1)
+    a.calorie_label.grid(row=4, column=1)
+    a.name_data_label.grid(row=1, column=2)
+    a.age_data_label.grid(row=2, column=2)
+    a.heart_data_label.grid(row=3, column=2)
+    a.calorie_data_label.grid(row=4, column=2)
+    a.heart_unit_label.grid(row=3, column=3)
+
+
+
+
+border=5
+frameBG='#000033'
+frameTC='#FFFFFF'
+frameFont='Helvetica 42'
+
+dist_meas = 0.00
+circ_cm = 188.62
+km_per_hour = 0
+rpm = 0
+elapse = 0
+sensor = 21
+pulse = 0
+start_timer = time.time()
+
+
+a = Tk()                                 #create window
+a.minsize(1490, 320)                      #size window 
+a.title("Current Statistics")            #title window
+a.configure(background=frameBG)        #background color   
+
+enter_name = Label(a, text="Name", font='Helvetica 50', bg=frameBG, fg=frameTC)   #create text variable
+enter_name.grid(row=1, column=1)
+
+name_entry = Entry(a, bd = 10, font='Helvetica 50')                   #create text entry box
+name_entry.grid(row=1, column=2)
+
+
+enter_age = Label(a, text="Age", font='Helvetica 50', bg=frameBG, fg=frameTC)   #create text variable
+enter_age.grid(row=2,column=1)
+
+age_entry = Entry(a, bd = 10, font='Helvetica 50')                   #create text entry box
+age_entry.grid(row=2, column=2)
+
+enter_button = Button(a, text = "ENTER", command=click, height = 5, width = 30) #create button
+enter_button.grid(row=3, column=2)
+
+
+a.mainloop()
