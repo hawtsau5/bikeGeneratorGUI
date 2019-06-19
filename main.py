629e71698f5dc36c67f61c64bcafd08546a9bdcb
Erlangga Ibrahim <erlanggaibr2@gmail.com>
2019-06-20 02:57:16 +0700

    lol

--- main.py
+++ main.py
@@ -3,25 +3,14 @@ from data_handler.datas import *
 from data_handler.sensors import *
 
 sensor = Sensor(21)
+# Data Parameters are --> NAME, AGE, WEIGHT, (TRUE FOR FEMALE, FALSE FOR MALE)
 data = Data("Wowotek", 22, 90, False)
+# Calculator Parameters Are --> DATA CLASS, SENSOR CLASS, WHEEL_CIRCUMFERENCE
 calculator = Calculator(data, sensor, 20)
-
-
-#------------------------------------------
-'''
-this is to test the program
-so basically it's a unended loop
-and looped every second
-it simulate sensor detected the rotation magnet sensors every second
-
-
-'''
 
 test_time = time.time()
 while True:
     calculator.calculate()
-    sensor.callback_function(21)
     data.pretty_print()
     
-
-    time.sleep(1)
+    time.sleep(.01)
