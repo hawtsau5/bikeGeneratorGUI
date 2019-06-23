6fc5ce013cb09795105716f83e53b8d24fd93fba
Erlangga Ibrahim <erlanggaibr2@gmail.com>
2019-06-23 21:40:05 +0700

    fixed the font

--- main.py
+++ main.py
@@ -5,16 +5,15 @@ from data_handler.calculation import *
 from gui.user_input_window import UserInputApplication
 from gui.gui import *
 
-# data = Data(name="", age=0, weight=0, gender="")
+data = Data(name="", age=0, weight=0, gender="")
 
-# uia = UserInputApplication(data)
-# uia.master.title("Please Provide Your Information, so we can hack your computer")
-# uia.master.maxsize(1366, 636)
-# uia.mainloop()
+uia = UserInputApplication(data)
+uia.master.title("Please Provide Your Information, so we can hack your computer")
+uia.master.maxsize(1366, 636)
+uia.mainloop()
 
-# if not data.validate(): exit()
+if not data.validate(): exit()
 
-data        = Data(name="Erlangga Ibrahim", age=22, weight=90, gender=False)
 sensor      = Sensor(21)
 calculator  = Calculator(data, sensor, 240)
 gui         = GUI(data)
